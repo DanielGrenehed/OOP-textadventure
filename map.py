b849bf6b2d6858ddca14899d120943bd9a9e26f7
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2021-10-06 18:07:41 +0200

    Commented map

--- map.py
+++ map.py
@@ -1,4 +1,3 @@
-
 import room
 
 # 
@@ -6,7 +5,7 @@ import room
 #
 #
 class Map:
-
+    ''' The map contans all the rooms the player has visited '''
     def __init__(self):
         self.pos_x = 0
         self.pos_y = 0
@@ -14,25 +13,31 @@ class Map:
         self.__createIfNotExists()
     
     def goNorth(self):
+        ''' moves position North and generates a room if there is`nt one '''
         self.pos_y += 1
         self.__createIfNotExists()
 
     def goSouth(self):
+        ''' moves position South and generates a room if there is`nt one '''
         self.pos_y -= 1
         self.__createIfNotExists()
 
     def goWest(self):
+        ''' moves position West and generates a room if there is`nt one '''
         self.pos_x -= 1
         self.__createIfNotExists()
 
     def goEast(self):
+        ''' moves position East and generates a room if there is`nt one '''
         self.pos_x += 1
         self.__createIfNotExists()
 
     def __createIfNotExists(self):
+        ''' Generates a room for the current position if there is`nt one already generated '''
         if (f"{self.pos_x} : {self.pos_y}") not in self.tiles:
             self.tiles[f"{self.pos_x} : {self.pos_y}"] = room.generateRoom()
         
 
     def getCurrentRoom(self):
+        ''' Returns the room object of the current position '''
         return self.tiles[f"{self.pos_x} : {self.pos_y}"]
