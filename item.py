6ab489925efe9bc60e1ecfe79c726a49d96d587c
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2021-10-06 17:55:59 +0200

    Commented item

--- item.py
+++ item.py
@@ -1,34 +1,38 @@
 import random
 
 class Item:
-
+    ''' Items have names and hardness, since all you can do with an item is attack somebody there is no reason to add more features to items '''
     def __init__(self, name, hardness):
         self.name = name
         self.hardness = hardness
 
     def getName(self):
+        ''' Returns the name of the item '''
         return self.name
 
     def getHardness(self):
+        ''' Returns the hardness of the item '''
         return self.hardness
 
-    def use(self):
-        return None
 
 class Sword(Item):
+    ''' Swords are hard items '''
     def __init__(self):
         super().__init__("Sword", 5)
 
 class Knife(Item):
+    ''' Knifes are less powerful than swords '''
     def __init__(self):
         super().__init__("Knife", 4)
 
 class Bottle(Item):
+    ''' Bottles are terrible weapons '''
     def __init__(self):
         super().__init__("Bottle", 2)
 
 
 def getRandomItem():
+    ''' Generates a random item '''
     i = random.randint(0, 2)
     if i == 0: return Sword()
     elif i == 1: return Knife()
